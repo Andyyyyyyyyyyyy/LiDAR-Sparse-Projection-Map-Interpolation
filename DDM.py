3ca5497889ddae4908b6590357b0ba1335c51f7a
Andyyyyyyyyyyyy <49580046+Andyyyyyyyyyyyy@users.noreply.github.com>
2020-05-21 18:45:57 +0800

    Update DDM.py

--- DDM.py
+++ DDM.py
@@ -1,24 +1,16 @@
 import os
 import os.path as osp
 import sys
-from collections import defaultdict
 import glob
-import collections
 import time
 import numpy as np
-import PIL.Image
 import cv2
 from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 
-#range
-min_range = 5
-max_range = 80
-
-
 img_dir ="./Images/"
 lidar_dir = "./LiDAR/"
-calib_file = "calibration.txt"   
+calib_dir = "./calib/"
 ouput_dir ="./DenseDepthMap/"
 
 if osp.exists(ouput_dir):
@@ -30,15 +22,17 @@ else:
 #read files
 img_files = glob.glob(osp.join(img_dir, '*.png'))
 lidar_files = glob.glob(osp.join(lidar_dir, '*.bin'))
+calib_files = glob.glob(osp.join(calib_dir, '*.txt'))
 img_files.sort()
 lidar_files.sort()
+calib_files.sort()
 
 def read_calib_file(filepath):
     """Read in a calibration file and parse into a dictionary."""
     data = {}
 
     with open(filepath, 'r') as f:
-        for line in f.readlines():
+        for line in f.readlines()[:7]:
             key, value = line.split(':', 1)
             # The only non-float values in these files are dates, which
             # we don't care about anyway
@@ -49,35 +43,37 @@ def read_calib_file(filepath):
 
     return data
 
-# read calibration.txt #another cali_matrix form
-calib = read_calib_file(calib_file)
-R_rect_00 = np.zeros((4, 4))
-R_rect_00[:3,:3] = np.array(calib['R0_rect']).reshape(-1, 3)
-R_rect_00[3,3] = 1.
+for index in range(len(lidar_files)):
 
-P_rect_02 = np.zeros((4, 4))
-P_rect_02[:3, :] = np.array(calib['P2']).reshape(-1, 4)
-P_rect_02[3, 3] = 1.
-
-vel2cam0 = np.zeros((4, 4))
-vel2cam0[:3,:] = np.array(calib['Tr_velo_to_cam']).reshape(-1, 4)
-vel2cam0[3,3] = 1.
-
-# compute transform matrix 
-T_lidar_camera = np.dot(R_rect_00, vel2cam0)
-P_lidar_image = np.dot(P_rect_02, T_lidar_camera)
-
-#load
-for  i, lidar_file  in enumerate(lidar_files):
-
-    print(i,'Generating dense depth image from:', osp.basename(lidar_file))
+    print(index,'Generating dense reflectivity image from:', osp.basename(lidar_files[index]))
     start_time = time.time()
-    
-    with open(lidar_file, 'r') as f:
-        pointcloud = np.fromfile(f, dtype=np.float32).reshape(-1, 4)
 
-    image = cv2.imread(img_files[i]) 
+    # read calibration files 
+    calib = read_calib_file(calib_files[index])
+    R_rect_00 = np.zeros((4, 4))
+    R_rect_00[:3,:3] = np.array(calib['R0_rect']).reshape(-1, 3)
+    R_rect_00[3,3] = 1.
+
+    P_rect_02 = np.zeros((4, 4))
+    P_rect_02[:3, :] = np.array(calib['P2']).reshape(-1, 4)
+    P_rect_02[3, 3] = 1.
+
+    vel2cam0 = np.zeros((4, 4))
+    vel2cam0[:3,:] = np.array(calib['Tr_velo_to_cam']).reshape(-1, 4)
+    vel2cam0[3,3] = 1.
+
+    # compute transform matrix 
+    T_lidar_camera = np.dot(R_rect_00, vel2cam0)
+    P_lidar_image = np.dot(P_rect_02, T_lidar_camera)
+    
+    # read images 
+    image = cv2.imread(img_files[index]) 
     h, w, _= image.shape
+    
+    with open(lidar_files[index], 'r') as f:
+        pointcloud = np.fromfile(f, dtype=np.float32).reshape(-1, 4)
+        
+    base = osp.splitext(osp.basename(lidar_files[index]))[0]
    
     pointcloud = pointcloud[np.logical_and((pointcloud[:, 1] < pointcloud[:, 0] - 0.27), (-pointcloud[:, 1] < pointcloud[:, 0] - 0.27))]   
     
@@ -94,35 +90,31 @@ for  i, lidar_file  in enumerate(lidar_files):
     pixel = pixel[ia,:]                                                     # [r c depth reflectance], 
 
     #sparse maps 
-    dms = np.zeros((h, w))                        # reflectance map
-    dms[pixel[:, 1].astype(np.uint16), pixel[:, 0].astype(np.uint16)] = pixel[:, 3]   
+    rms = np.zeros((h, w))                        # reflectance map
+    rms[pixel[:, 1].astype(np.uint16), pixel[:, 0].astype(np.uint16)] = pixel[:, 3]   
 
 
     # interpolation reflectance-map 
     row, col = np.meshgrid(np.linspace(0, w-1, num=w), np.linspace(0, h-1, num=h))  
-    dmd = griddata(pixel[:, :2], pixel[:, 2], (row, col), method='nearest')    #try 'nearest','linear'
-
-    dmd = (dmd - min_range) / (max_range - min_range)
-    dmd = np.where(dmd < 0, 0, dmd)
-    dmd = np.where(dmd > 1, 1, dmd)
+    rmd = griddata(pixel[:, :2], pixel[:, 3], (row, col), method='linear')    #try 'nearest','linear'
 
     # mask the result to the ROI
     mask = np.zeros((h, w))                                # initialize mask 
-    index = (dms != 0).argmax(axis=0)
+    ind = (rms != 0).argmax(axis=0)
     for i in range(w):
-        mask[index[i]:, i] = 1
+        mask[ind[i]:, i] = 1
 
     kernel = np.ones((2, 10), np.uint8)
     mask   = cv2.dilate(mask,kernel,iterations = 1)                         # by Dilation
     
     ## mask the reflectance map
-    dm  = mask*(255*dmd).astype(np.uint8)
+    rm  = mask*(255*rmd).astype(np.uint8)
     
     #Save
-    plt.imsave(ouput_dir+osp.splitext(osp.basename(lidar_file))[0]+".jpg", dm, cmap="jet")
-#    cv2.imwrite(ouput_dir+osp.splitext(osp.basename(lidar_file))[0]+".jpg", dm)
+    plt.imsave(ouput_dir+base+".jpg", rm, cmap="jet")   
+#    cv2.imwrite(ouput_dir+base+".jpg", rm)
     
-    print("Saving as :",ouput_dir+osp.splitext(osp.basename(lidar_file))[0]+".jpg")
+    print("Saving as :",base+".jpg")
     end_time = time.time()
     print("Time Consuming :", end_time - start_time)
     print()
